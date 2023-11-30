c4f45d85f8900e4e9c26bdd561523a605e1671a8
김선우 <frod5@kakao.com>
2023-11-30 22:07:19 +0900

    자바 베이직 chapter1

---
+++ src/CommentJava.java
@@ -0,0 +1,6 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // hello java 출력
+//        System.out.println("Hello Java2");
+    }
+}
