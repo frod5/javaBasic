3ea12ae56568c519fe404ef59ffde88c9c37fc73
김선우 <frod5@kakao.com>
2023-11-30 22:36:45 +0900

    자바 베이직 chapter2 실습 및 정리

---
+++ src/variable/Var6.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        //만약에 변수를 초기화하지 않으면 어떻게 될까?
+        //쓰레기값 들어가있을 수도 있기 떄문
+        //자바는 초기화가 강제 안그러면 컴파일 오류
+        int a;
+//        System.out.println(a); // variable a might not have been initialized
+    }
+}
